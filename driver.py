adaa34db3d8258ae063b606426f2a48761d672f3
Ethan Cheung <54164738+echeung801@users.noreply.github.com>
2022-05-25 22:36:58 -0400

    Update driver.py

--- driver.py
+++ driver.py
@@ -1,2 +1,10 @@
+"""
+File:         Driver.py
+Author:       Ethan Cheung, Antionne Andries, Manuela Rugemintgwaza
+Date:         5/25/2022
+E-mail:       echeung801@gmail.com, xxxxxx
+Description:  This is the driver code.
+"""
+
 if __name__ == '__main__':
   print("This is the driver code")
