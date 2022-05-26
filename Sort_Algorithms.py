25652b9bd3c863812132b68a70b795e10390bdb9
Ethan Cheung <54164738+echeung801@users.noreply.github.com>
2022-05-25 23:07:57 -0400

    Update Sort_Algorithms.py

--- Sort_Algorithms.py
+++ Sort_Algorithms.py
@@ -1,10 +1,14 @@
 
+DATA = []
 
+def radix_sort(DATA):
 
-def bubble_sort(data):
+def bubble_sort(DATA):
   
-def quick_sort(data):
+def quick_sort(DATA):
  
-def merge_sort(data):
+def merge_sort(DATA):
   
-def insertion_sort(data):
+def insertion_sort(DATA):
+
+def comb_sort(DATA):
